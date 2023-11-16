2e3298565fb7f1d5894a10750828e84c87dcc4e1
g21turbo <dg21stew@gmail.com>
2023-11-15 22:43:10 -0800

    completed while loop

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        inputThenPrintSumAndAverage();
 
     }
 
     public static void inputThenPrintSumAndAverage() {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+        long avg = 0;
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int num = scanner.nextInt();
+                sum += num;
+                count++;
+            } else {
+                break;
+            }
+
+
+
+        }
+        System.out.println(number);
+
     }
 
 
